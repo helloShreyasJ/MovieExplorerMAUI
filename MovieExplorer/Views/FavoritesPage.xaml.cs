b9ca7bfb3ef09d0893a6b0b50e716fcf80c51efb
Shreyas <helloshreyasj@gmail.com>
2025-11-25 10:04:26 +0000

    feat: add TabBar

---
+++ MovieExplorer/Views/FavoritesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MovieExplorer.Views;
+
+public partial class FavoritesPage : ContentPage
+{
+    public FavoritesPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MovieExplorer/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MovieExplorer.Views;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
