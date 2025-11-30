b528edcbd1f4b5a6e7a393231a747d7f90a7a828
Shreyas <helloshreyasj@gmail.com>
2025-11-30 11:21:48 +0000

    chore: add movie model

---
+++ MovieExplorer/Models/Movie.cs
@@ -0,0 +1,11 @@
+namespace MovieExplorer.Models;
+
+public class Movie
+{
+    public string title { get; set; }
+    public int year { get; set; }
+    public List<string> genre { get; set; }
+    public string director { get; set; }
+    public double rating { get; set; }
+    public string emoji { get; set; }
+}
