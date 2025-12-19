04a93fbe0c556322dfd69da9b4e5277e62b78fb9
Shreyas <helloshreyasj@gmail.com>
2025-12-19 19:32:28 +0000

    feat: add dark theme

--- MovieExplorer/App.xaml.cs
+++ MovieExplorer/App.xaml.cs
@@ -1,14 +1,23 @@
-﻿namespace MovieExplorer;
+﻿using MovieExplorer.Resources.Styles;
+
+namespace MovieExplorer;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
+        SetInitialTheme();
     }
     
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell());
+    }
+    
+    private void SetInitialTheme()
+    {
+        Application.Current.Resources.MergedDictionaries.Add(new LightTheme());
+        Application.Current.UserAppTheme = AppTheme.Light;
     }
 }
--- MovieExplorer/Views/Settings.xaml.cs
+++ MovieExplorer/Views/Settings.xaml.cs
@@ -1,7 +1,12 @@
+using MovieExplorer.Resources.Styles;
+
 namespace MovieExplorer.Views;
 
 public partial class Settings : ContentPage
 {
+    private static readonly LightTheme LightTheme = new LightTheme();
+    private static readonly DarkTheme DarkTheme =  new DarkTheme();
+    
     public Settings()
     {
         InitializeComponent();
@@ -9,6 +14,33 @@ public partial class Settings : ContentPage
 
     private void DarkMode_OnToggled(object? sender, ToggledEventArgs e)
     {
+        if (e.Value)
+        {
+            ChangeTheme();
+        }
+        else
+        {
+            ChangeTheme();
+        }
+    }
+
+    public static AppTheme ChangeTheme()
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (Application.Current.UserAppTheme == AppTheme.Dark)
+        {
+            Application.Current.UserAppTheme = AppTheme.Light;
+            mergedDictionaries.Remove(DarkTheme);
+            mergedDictionaries.Add(LightTheme);
+        }
+        else
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark;
+            mergedDictionaries.Remove(LightTheme);
+            mergedDictionaries.Add(DarkTheme);
+            
+        }
         
+        return Application.Current.UserAppTheme;
     }
 }
